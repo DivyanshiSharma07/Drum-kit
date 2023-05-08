8de0915b73703025ebd1a1522679f450e330cb9e
DivyanshiSharma07 <127393402+DivyanshiSharma07@users.noreply.github.com>
2023-05-08 15:10:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ document.addEventListener("keypress",function(event){
 function makeSound(key){
     switch (key) {
         case "w":
-            var audio = new Audio("sounds/sounds_tom-1.mp3");
+            var audio = new Audio("sounds_tom-1.mp3");
             audio.play();   
             break;
         case "a":
-            var audio = new Audio("sounds/sounds_tom-2.mp3");
+            var audio = new Audio("sounds_tom-2.mp3");
             audio.play();   
             break;
         case "s":
-            var audio = new Audio("sounds/sounds_tom-3.mp3");
+            var audio = new Audio("sounds_tom-3.mp3");
             audio.play();   
             break;
         case "d":
-            var audio = new Audio("sounds/sounds_tom-4.mp3");
+            var audio = new Audio("sounds_tom-4.mp3");
             audio.play();   
             break;
         case "j":
-            var audio = new Audio("sounds/sounds_crash.mp3");
+            var audio = new Audio("sounds_crash.mp3");
             audio.play();   
             break; 
         case "k":
-            var audio = new Audio("sounds/sounds_snare.mp3");
+            var audio = new Audio("sounds_snare.mp3");
             audio.play();   
             break; 
         case "l":
-            var audio = new Audio("sounds/sounds_kick-bass.mp3");
+            var audio = new Audio("sounds_kick-bass.mp3");
             audio.play();   
             break;  
         default:
